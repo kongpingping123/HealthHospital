c54c35ef942cef8326b1ebc8129b3e3f1859a2a5
kongpingping123 <1294042619@qq.com>
2019-12-10 15:57:43 +0800

    啦啦啦啦啦

--- common/src/main/java/com/wd/common/core/http/IAppRequest.java
+++ common/src/main/java/com/wd/common/core/http/IAppRequest.java
@@ -6,12 +6,14 @@ import com.wd.common.bean.CommBase;
 import com.wd.common.bean.DiseBase;
 import com.wd.common.bean.DonBase;
 import com.wd.common.bean.DrugBase;
+import com.wd.common.bean.DtonBase;
 import com.wd.common.bean.IntBase;
 import com.wd.common.bean.CircleBean;
 import com.wd.common.bean.DepartmentBean;
 import com.wd.common.bean.DetailsBean;
 import com.wd.common.bean.PatientBean;
 import com.wd.common.bean.Result;
+import com.wd.common.bean.SousuoBase;
 import com.wd.common.bean.XiangqingBase;
 import com.wd.common.bean.YaoBase;
 import com.wd.common.bean.ZixunBean;
@@ -91,4 +93,10 @@ public interface IAppRequest {
           Observable<Result<DonBase>>donfindduknedge(@Query("id") int id);
              @GET("share/knowledgeBase/v1/findDrugsKnowledge")
            Observable<Result<IntBase>>intduiokonggen(@Query("id")int id);
+
+             @GET("share/information/v1/findInformation")
+           Observable<Result<DtonBase>>deisebase(@Query("infoId")int infoId);
+
+             @GET("share/v1/homePageSearch")
+            Observable<Result<List<SousuoBase>>>sousuohomepage(@Query("keyWord")String keyWord);
 }
---
+++ health_main/src/main/java/com/wd/health_main/activity/DeiserActivity.java
@@ -0,0 +1,89 @@
+package com.wd.health_main.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.wd.common.bean.DtonBase;
+import com.wd.common.core.DataCall;
+import com.wd.common.core.WDActivity;
+import com.wd.common.core.exception.ApiException;
+import com.wd.health_main.R;
+import com.wd.health_main.R2;
+import com.wd.health_main.fragment.CinemaFragment;
+import com.wd.health_main.presenter.DisePresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class DeiserActivity extends WDActivity {
+
+    @BindView(R2.id.jing1)
+    TextView jing1;
+    @BindView(R2.id.jing2)
+    TextView jing2;
+    @BindView(R2.id.simple_view)
+    SimpleDraweeView simpleView;
+    @BindView(R2.id.jing3)
+    TextView jing3;
+    @BindView(R2.id.imager3)
+    ImageView imager3;
+    private DisePresenter disePresenter;
+    private int id;
+
+    @Override
+    protected int getLayoutId() {
+
+        return R.layout.dijing_layout;
+    }
+
+    @Override
+    protected void initView() {
+        disePresenter = new DisePresenter(new dijing());
+        Intent intent = getIntent();
+        id = intent.getIntExtra("id", 0);
+        disePresenter.reqeust(id);
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+
+
+
+    @OnClick(R2.id.imager3)
+    public void onClick() {
+           finish();
+
+    }
+
+
+    class dijing implements DataCall<DtonBase> {
+
+        private Uri parse;
+
+        @Override
+        public void success(DtonBase data, Object... args) {
+
+            Log.d("aaa", "success: " + data);
+
+            jing1.setText(data.source);
+            /*parse = Uri.parse(data.whetherCollection);
+            simpleView.setImageURI(parse);*/
+            jing2.setText(data.title);
+            jing3.setText(data.whetherCollection);
+
+        }
+
+        @Override
+        public void fail(ApiException data, Object... args) {
+
+        }
+    }
+}
